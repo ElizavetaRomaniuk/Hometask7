92681a3338f36317df3a648198ca89fe14782f80
Elizaveta Romaniuk <elizavetasiniuk@gmail.com>
2023-03-06 14:09:00 +0300

    Task 1 done

---
+++ Task1/Program.cs
@@ -0,0 +1,19 @@
+﻿// Задайте двумерный массив размером m×n, заполненный случайными вещественными числами.
+Console.WriteLine("Input m");
+string example1 = Console.ReadLine();
+int m = Convert.ToInt32(example1);
+Console.WriteLine("Input n");
+string example2 = Console.ReadLine();
+int n = Convert.ToInt32(example2);
+
+int[,] array = new int[m, n];
+
+for (int i = 0; i < array.GetLength(0); i++)
+{
+Console.WriteLine();
+for (int j = 0; j < array.GetLength(1); j++)
+{
+array[i, j] = new Random().Next(-10, 11);
+Console.Write(array[i, j] + " ");
+}
+}
